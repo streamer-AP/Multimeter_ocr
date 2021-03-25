1d7988045a4d199ff64a2cc74cfad2bf6bc6c1fe
cryan <37429186+streamer-AP@users.noreply.github.com>
2021-03-25 17:06:32 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import cv2
 import imutils
 import numpy as np
-
+import torch
+from PIL import  Image
+from torchvision.transforms import ToTensor
 DIGITS_LOOKUP = {
     (1, 1, 1, 0, 1, 1, 1): 0,
     (0, 0, 1, 0, 0, 1, 0): 1,
@@ -15,6 +17,16 @@ DIGITS_LOOKUP = {
     (1, 1, 1, 1, 1, 1, 1): 8,
     (1, 1, 1, 1, 0, 1, 1): 9
 }
+
+
+def color_transfer(src,dst):
+    src=cv2.cvtColor(src,cv2.COLOR_BGR2LAB)
+    dst=cv2.cvtColor(dst,cv2.COLOR_BGR2LAB)
+
+    src_mean,src_std=cv2.meanStdDev(src)
+    dst_mean,dst_std=cv2.meanStdDev(dst)
+    src,dst=src.astype(np.float32),dst.astype(np.float32)
+    src=(src-src_mean)*(dst_std/src_std)+dst_mean
 
 
 def get_binary_img(img, method=cv2.THRESH_OTSU, inv=True, low=50, high=255):
@@ -64,51 +76,45 @@ def check_minus(binary_img, digit_top, digit_height):
             check = True
             minus_pos = [x, y, w, h]
     return check, minus_pos
+def get_digit_imgs(img,top_ratio,bottom_ratio,pre_contour_box,pre_char_num,pre_char_width,pre_first_char_offset=20):
+    height, width = img.shape[:-1]
+    mid_roi = [slice(int(height*top_ratio), int(height*bottom_ratio)), slice(0, width)]
 
-def digit_recognition(binary_img, digit_contours_bbox, x_offset=0, y_offset=0):
-    width = binary_img.shape[1]
-    digit_top = int(
-        sum([box[1] for box in digit_contours_bbox])/len(digit_contours_bbox))
-    digit_height = int(
-        sum([box[3] for box in digit_contours_bbox])/len(digit_contours_bbox))
-    digits = []
-    check, pos = check_minus(binary_img, digit_top, digit_height)
-    if check:
-        digits.append("-")
-    for (x, y, w, h) in digit_contours_bbox:
-        x = x+x_offset
-        y = y+y_offset
-        roi = binary_img[y:y + h, x:x + w]
-        segments = get_segment(w, h)
-        on = [0] * len(segments)
-        if w > 0.1*width:
-            for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
-                yB = max(yA+1, yB)
-                xB = max(xA+1, xB)
-                segROI = roi[yA:yB, xA:xB]
-                char_area = cv2.countNonZero(segROI)
-                area = (xB - xA) * (yB - yA)
-                if (char_area+0.0001) / (float(area)+0.001) > 0.4:
-                    on[i] = 1
-        digit = DIGITS_LOOKUP.get(tuple(on), "unkwon")
-        digits.append(str(digit))
-    return pos, digits
+    mid_region = img[mid_roi[0], mid_roi[1]].copy()
+    mid_region_gray = cv2.cvtColor(mid_region, cv2.COLOR_BGR2GRAY)
+
+    digit_contours_bbox = digit_split(mid_region_gray,pre_first_char_offset,pre_char_num,pre_char_width,pre_contour_box=pre_contour_box)
+    digit_imgs=[mid_region[v[1]:v[1]+v[3],v[0]:v[0]+v[2]] for v in digit_contours_bbox ]
+
+    return digit_imgs
+def get_digits(imgs,model,char_dict):
+    digits=[]
+    with torch.no_grad():
+        for img in imgs:
+            img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            input_tensor=ToTensor(cv2.resize(img,(40,80))).unsqueeze(0)
+            output=model(input_tensor).detach().cpu().numpy()
+            digits.append(char_dict[np.argmax(output[0])])
+    return digits
+
+def get_unit(img,model,transform,char_dict):
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img=Image.fromarray(img)
+    input_tensor=transform(img)
+    with torch.no_grad():
+        output=model(input_tensor.unsqueeze(0)).detach().cpu().numpy()
+        print(output)
+    return char_dict[np.argmax(output[0])]
 
 
-def digit_split(binary_img, pre_first_char_offset=20, char_num=4, pre_char_width=50, post_process=None):
-
-    if post_process != None:
-        binary_img = post_process(binary_img)
-    height, width = binary_img.shape[0], binary_img.shape[1]
-    contours = get_contours(binary_img.copy())
-    contours_bbox = get_contours_bbox(contours)
-
-    vertical_projection = np.sum(binary_img, axis=0)
+def digit_split(gray_img,pre_first_char_offset=20, char_num=4, pre_char_width=50, post_process=None, pre_contour_box=None):
+    height,width=gray_img.shape[0],gray_img.shape[1]
+    vertical_projection = np.sum(255-gray_img, axis=0)
     min_background_area = np.Inf
     best_char_width = pre_char_width
     best_first_char_offset = pre_first_char_offset
-    for char_width in range(pre_char_width-5, pre_char_width+5):
-        for first_char_offset in range(pre_first_char_offset):
+    for char_width in range(max(0,pre_char_width-10), pre_char_width+10):
+        for first_char_offset in range(max(0,pre_first_char_offset-20),pre_first_char_offset):
             if char_num*char_width+first_char_offset < width:
                 background_area = np.sum(
                     vertical_projection[first_char_offset::char_width])
@@ -116,24 +122,12 @@ def digit_split(binary_img, pre_first_char_offset=20, char_num=4, pre_char_width
                     min_background_area = background_area
                     best_char_width = char_width
                     best_first_char_offset = first_char_offset
-    avg_digit_top = 0
-    avg_digit_height = 0
-    visable_digit = 0
-    for (x, y, w, h), contour in zip(contours_bbox, contours):
-        if y < height*0.25 and w < width*0.25 and h > height*0.5 and w > width*0.05:
-            avg_digit_top += y
-            avg_digit_height += h
-            visable_digit += 1
-    if visable_digit > 0:
-        avg_digit_height /= visable_digit
-        avg_digit_top /= visable_digit
-        contours_bbox = [(best_first_char_offset+best_char_width*idx, int(avg_digit_top),
-                          best_char_width, int(avg_digit_height)) for idx in range(char_num)]
-    else:
-        contours_bbox = []
-    return contours_bbox, binary_img
 
-def check_dot(binary_img, digit_contours_bbox,max_dot_digit_dist=20,dot_area_min=20,dot_area_max=120):
+    contours_bbox = [(max(0,best_first_char_offset+int(best_char_width*(idx-0.2))), 0,
+                          int(best_char_width*1.4),height) for idx in range(char_num)]
+    return contours_bbox
+
+def check_dot(binary_img, digit_contours_bbox,max_dot_digit_dist=20,dot_area_min=20,dot_area_max=120, debug=False):
     contours = get_contours(binary_img)
     contours_bbox = get_contours_bbox(contours)
     digit_bottom = int(
@@ -141,13 +135,18 @@ def check_dot(binary_img, digit_contours_bbox,max_dot_digit_dist=20,dot_area_min
     digit_right = max([box[0] for box in digit_contours_bbox])
     dot_x = -1
     best_dist = max_dot_digit_dist
+    contour_box = None
     for (x, y, w, h) in contours_bbox:
         if dot_area_min < w*h < dot_area_max and abs(y+h-digit_bottom) < best_dist and x < digit_right:
             best_dist = abs(y+h-digit_bottom)
             dot_x = x
-    dot_idx = 0
+            contour_box = (x,y,w,h) 
+    dot_idx = -1
     if dot_x != -1:
         for idx, bbox in enumerate(digit_contours_bbox):
             if abs(bbox[0]-dot_x) < abs(digit_contours_bbox[dot_idx][0]-dot_x):
                 dot_idx = idx
-    return dot_idx
+    if debug:
+        return dot_idx, contour_box
+    else:
+        return dot_idx
